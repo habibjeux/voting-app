429bdbf509805dec84232725a83ec4c27074f4c5
Habib <habibjeux@gmail.com>
2024-05-10 10:55:54 +0000

    Spring initializr

---
+++ src/main/java/com/ucad/m2sir/votingapp/VotingAppApplication.java
@@ -0,0 +1,13 @@
+package com.ucad.m2sir.votingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotingAppApplication.class, args);
+	}
+
+}
