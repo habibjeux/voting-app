ad0d7ca0cb4bf47271c42476c6ba1b2aeaae7e20
zoudoum <doumzoro@gmail.com>
2024-05-11 21:12:03 +0000

    mise en place projet maven

--- src/main/java/com/ucad/m2sir/votingapp/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ucad.m2sir.votingapp.repository;
-
-import com.ucad.m2sir.votingapp.entite.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long>{
-}
